6f3d807ccdeb9d60f95abffd6c0c9df46c97fb7a
Daniel Solano Gómez <daniel@solanogomez.org>
2012-07-14 13:18:54 -0500

    Prep for next release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-tarsier "0.9.3"
+(defproject lein-tarsier "0.9.3-SNAPSHOT"
   :description "Launches a VimClojure server for Vim."
   :url "https://github.com/sattvik/lein-tarsier"
   :license {:name "Eclipse Public License"
