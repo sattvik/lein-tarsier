5ebcfc8d4a53f5855554c91f765c1be8f13437d5
Daniel Solano Gómez <daniel@solanogomez.org>
2012-10-14 13:52:38 -0500

    Update leinjacker dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/core.contracts "0.0.1"]
-                 [leinjacker "0.2.0"]]
+                 [leinjacker "0.3.1"]]
   :min-lein-version "2.0.0"
   :eval-in-leiningen true)
