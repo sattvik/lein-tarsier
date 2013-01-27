cb9c4ebb8ec430a9da9bae3edc1ba3f578e61ede
Daniel Solano Gómez <daniel@solanogomez.org>
2013-01-27 08:55:57 -0600

    Back to development mode

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-tarsier "0.10.0"
+(defproject lein-tarsier "0.10.1-SNAPSHOT"
   :description "Launches a VimClojure server for Vim."
   :url "https://github.com/sattvik/lein-tarsier"
   :license {:name "Eclipse Public License"
